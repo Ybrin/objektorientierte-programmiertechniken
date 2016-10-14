7862796c5a15c3007e584589dbf6a26f90a15a7a
Ybrin <koray@koska.at>
2016-10-14 22:30:51 +0200

    Add group size reset

--- Aufgabe1-3/src/Test.java
+++ Aufgabe1-3/src/Test.java
@@ -32,10 +32,14 @@ public class Test {
         for (Group group : groups) {
             System.out.println("******* The following group has the " + group.getStrategy().toString() + " Strategy! *******");
             System.out.println("  * Starting with a group size of " + group.getGroupsize());
+
+            int oldSize = group.getGroupsize();
             for (int i = 0; i < 10; i++) {
                 group.simulateYearPass();
                 System.out.println(i + ": " + group.getGroupsize());
             }
+            // Reset group size to have a unique simulation for the other groups
+            group.setGroupsize(oldSize);
             System.out.println("******* NEXT *******");
         }
     }
