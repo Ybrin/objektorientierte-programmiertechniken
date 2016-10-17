12034ebaa84fe732d871a775e2fdbf2694868c0f
Aniela <e1456160@student.tuwien.ac.at>
2016-10-17 16:09:11 +0200

    Wiederherstelung von Sabines Gui Tabelle

--- Aufgabe1-3/src/Test.java
+++ Aufgabe1-3/src/Test.java
@@ -1,47 +1,43 @@
 import population.Group;
 import population.Population;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 /**work dedivision:
  * Koray:
- *konzeptentwicklung
- *Grundgerüst der klassen(mit getter und setter)
- *Calculate Klasse überarbeitet
- *Test Klasse den Test implementiert
- *
+ * konzeptentwicklung
+ * Grundgerüsst
  * Sabrina:
  *konzeptentwicklung
- *Grundgerüst der klassen(mit getter und setter)
- *Strategie verteilung
  *
  * Aniela:
  *konzeptentwicklung
  *erstimplementierung Methoden der Calulate Klasse
- *Test klasse ausgabe in Tabellenform
+ *Test klasse ausgabe
  *
  */
-public class Test {
+public class Test extends JFrame{
 
-    public Test(int j, List<Group> groupList) {
+    JTable table;
 
-        Group group = groupList.get(j);
-        System.out.println();
-        System.out.println("Group " + (j+1) + " uses " + group.getStrategy() + "!");
+    public Test() {
+        setLayout(new FlowLayout());
 
-        int oldSize = group.getGroupsize();
-        System.out.format("%20s%20s%20s%20s%20s%20s%20s","Year","Collected_Food", "Avalable_food", "Recovered_own_food","Found_Foreign_Food","Deaths" ,"Groupsize");
-        for (int i = 1; i <= 10; i++) {
-            group.simulateYearPass();
-            System.out.println();
-            System.out.format("%20s%20s%20s%20s%20s%20s%20s", i, group.getCollectedFoodPerIndividual(), group.getHuntableFoodPerIndiviual(), (int) (group.getRecoveredOwnFood() * 100) + "%",
-                    (int) (group.getFoundForeignFood() * 100) + "%", (int) (group.getDeathRate() * 100) + "%", group.getGroupsize());
+        String[] columns={"Year", "Collected amout of food/S", "Available food/S",
+            "Recoverd own food", "Found foreign food", "Death rate/S", "Groupsize"};
 
-        }
-        // Reset group size to have a unique simulation for the other groups
-        group.setGroupsize(oldSize);
+        Object[][] data={};
+
+        table= new JTable(new DefaultTableModel(data, columns));
+        table.setPreferredScrollableViewportSize(new Dimension(600,500));
+        table.setFillsViewportHeight(true);
+
+        JScrollPane scrollpane= new JScrollPane(table);
+        add(scrollpane);
     }
 
     public static void main(String[] args) {
@@ -63,10 +59,37 @@ public class Test {
 
         Population population = new Population(groups);
 
+        Test gui= new Test();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setSize(800,600);
+        gui.setVisible(true);
+        gui.setTitle("Squirrel Statistics");
+        DefaultTableModel model= (DefaultTableModel) gui.table.getModel();
+
+
         List<Group> groupList = population.getGroups();
 
         for (int j = 0; j < groupList.size(); j++){
-            new Test(j, groupList);
+            Group group = groupList.get(j);
+            model.addRow(new Object[]{"Group" +(j+1)});
+
+            System.out.println();
+           System.out.println("Group " + (j+1) + " uses " + group.getStrategy() + "!");
+
+            int oldSize = group.getGroupsize();
+            System.out.format("%20s%20s%20s%20s%20s%20s%20s","Year","Collected_Food", "Avalable_food", "Recovered_own_food","Found_Foreign_Food","Deaths" ,"Groupsize");
+            for (int i = 1; i <= 10; i++) {
+                group.simulateYearPass();
+                model.addRow(new Object[]{i, group.getCollectedFoodPerIndividual(), group.getHuntableFoodPerIndiviual(), (int) (group.getRecoveredOwnFood() * 100) + "%",
+                        (int) (group.getFoundForeignFood() * 100) + "%", (int) (group.getDeathRate() * 100) + "%", group.getGroupsize()});
+
+             System.out.println();
+                System.out.format("%20s%20s%20s%20s%20s%20s%20s", i, group.getCollectedFoodPerIndividual(), group.getHuntableFoodPerIndiviual(), (int) (group.getRecoveredOwnFood() * 100) + "%",
+                        (int) (group.getFoundForeignFood() * 100) + "%", (int) (group.getDeathRate() * 100) + "%", group.getGroupsize());
+
+            }
+            // Reset group size to have a unique simulation for the other groups
+            group.setGroupsize(oldSize);
         }
         System.out.println();
         System.out.println("Different Strategies: " + population.getDifferentStrategies());
