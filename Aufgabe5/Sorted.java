ead2c1d18531c6cc436acfab15a8ab30c8da2547
Aniela <e1456160@student.tuwien.ac.at>
2016-11-29 18:43:56 +0100

    updated Sorted

--- Aufgabe5/Sorted.java
+++ Aufgabe5/Sorted.java
@@ -1,5 +1,5 @@
-import java.util.Iterator;
-import java.util.Spliterator;
+import java.lang.reflect.Type;
+import java.util.*;
 import java.util.function.Consumer;
 
 /**
@@ -7,42 +7,93 @@ import java.util.function.Consumer;
  */
 public class Sorted implements java.lang.Iterable {
 
-    Iterator iterator;
 
+
+    Iterator iterator;
+    List <Before> beforeList;
+
+    /**
+     * Construkor
+     * @param iterator
+     * @autor Aniela
+     */
     public Sorted(Iterator iterator) {
-        this.iterator = iterator;
+
+        beforeList=new ArrayList<>();
+        this.iterator = beforeList.iterator();
     }
 
+    /**
+     * addes Before object into list
+     * @param before
+     * @autor aniela
+     */
     public void add(Before before){
 
+        beforeList.add( before);
+        //iterator=beforeList.iterator();
     }
 
+    /**
+     * removes Object from iterator
+     * @param before
+     * @autor aniela
+     */
     public void remove(Before before){
 
-        Before first=(Before) this;
-        Before help;
         while (iterator.hasNext()){
-            Before secund = (Before)iterator.next();
-            if(first.equals(before)){
-                iterator.next();
 
+            if(iterator.next().equals(before)){
+                iterator.remove();
             }
-            help= first;
-            first=secund;
         }
     }
 
+    /**
+     *
+     * creats new iterator
+     *
+     * @return iterator
+     * @autor Aniela
+     */
     @Override
     public Iterator iterator() {
-        Before x = (Before) this;
-        while (iterator.hasNext()){
-            Before y = (Before)iterator.next();
-            if(x.before(y)){
+        Iterator<Before> iterator = new Iterator<Before>() {
 
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < beforeList.size() && beforeList.get(currentIndex)!= null;
             }
-           x=y;
-        }
-       return iterator;
+
+            @Override
+            public Before next() {
+                Before before=beforeList.get(currentIndex);
+               /* if(beforeList.get(currentIndex).before(beforeList.get(currentIndex+1))){
+                   before= beforeList.get(currentIndex);
+                }else{
+                    before= beforeList.get(currentIndex+1);
+                }*/
+                return before;
+            }
+
+            @Override
+            public void remove() {
+                beforeList.set(currentIndex,beforeList.get(currentIndex+1));
+                for(int current = currentIndex+1; current<beforeList.size(); current++){
+                    if(beforeList.get(current+1).before(beforeList.get(current))){
+                        beforeList.set(current+1,beforeList.get(current+2));
+                        beforeList.set(current,beforeList.get(current+1));
+                    }else{
+                        beforeList.set(current,beforeList.get(current+1));
+                        beforeList.set(current+1,beforeList.get(current+2));
+                    }
+                }
+            }
+        };
+
+        return iterator;
     }
 
     @Override
