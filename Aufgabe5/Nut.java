277d1755fea2265da3796d31404925d1b5a35f13
Sabrina <e1425610@student.tuwien.ac.at>
2016-11-29 18:35:38 +0100

    adjusted SquirelRanking, SquirelFood, Fruit and Nut and added comments in SquirelRanking ans SquirelFood

--- Aufgabe5/Nut.java
+++ Aufgabe5/Nut.java
@@ -5,8 +5,9 @@ public class Nut extends SquirelFood {
 
     private char nutshell;
 
-    public Nut(double energy, double fat){
+    public Nut(double energy, double fat, char nutshell){
         super(energy, fat);
+        this.nutshell = nutshell;
     }
 
 }
--- Aufgabe5/SquirelFood.java
+++ Aufgabe5/SquirelFood.java
@@ -1,7 +1,7 @@
 /**
  * Created by Sabrina on 26.11.2016.
  */
-public abstract class SquirelFood { // implements Before <...>{
+public abstract class SquirelFood implements Before <SquirelFood>{
 
     protected final double energy;
     protected final double fat;
@@ -11,8 +11,13 @@ public abstract class SquirelFood { // implements Before <...>{
         this.fat = fat;
     }
 
-
-    private boolean before(SquirelFood y){
+    /**
+     * calculates if the energy or the fat of x is at least 20% higher than the energy or the fat of y
+     *
+     * @param  y another SquirelFood
+     * @return
+     */
+    public boolean before(SquirelFood y){
 
         return this.fat >= 1.2*y.fat || this.energy >= 1.2*y.energy;
 
